13cd96c7a3a27b43072689aa924a3d9cb9b3547b
nick <69575053+nickoncrack@users.noreply.github.com>
2026-09-15 20:31:16 +0300

    feat: add common.h with essential definitions

---
+++ src/include/common.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define MAX_BUFF 128
+
+#define EXIT_SUCCESS  0
+#define EXIT_CMD_NF   1 // Command not found
+
+
+enum env_var_type {
+  ENV_TYPE_STR, // string
+  ENV_TYPE_INT  // integer
+};
+
+typedef struct {
+  char name[64];
+  uint64_t value;
+  enum env_var_type type;
+} EnvironmentVariable;
