4f9cc38ed621f8353530af9c1c31029885028952
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-09-16 18:59:41 +0400

    chore: create hostname.h and add hostname macro to hostname.h

--- src/main.c
+++ src/main.c
@@ -2,14 +2,14 @@
 #include <string.h>
 #include "include/bootloader.h"
 #include "include/version.h"
+#include "include/hostname.h"
 int main(){
     bootloader();
     char user_input[50] = "";
     char larper_whoami[20] = "master larper";
-    char hostname[20] = "larper";
     printf("Welcome To Larp OS\n");
     while (1){
-        printf("larp-os@%s $ ",hostname);
+        printf("larp-os@%s $ ",HOSTNAME);
         fgets(user_input,sizeof(user_input),stdin);
         user_input[strcspn(user_input,"\n")] = '\0';
         if (strcmp(user_input,"exit") == 0){
