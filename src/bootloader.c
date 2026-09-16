6cbda234d91a1a07053ab4bb3d6c2e4081e8d71a
nick <69575053+nickoncrack@users.noreply.github.com>
2026-09-16 18:58:22 +0300

    fix: optimize bootloader input method using getch()

--- src/bootloader.c
+++ src/bootloader.c
@@ -2,30 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 #include "include/bootloader.h"
+
+
 void bootloader(){
     int bootloader_option;
-    char input_buffer[20] = "";
+    
     printf("Welcome To Larp OS LarpLoader (Bootloader)\n");
-
     printf("Boot Options\n");
     printf("1 : Larp OS\n");
     printf("Choose Boot Option\n $ ");
-    fgets(input_buffer,sizeof(input_buffer),stdin);
-    input_buffer[strcspn(input_buffer,"\n")] = '\0';
-    bootloader_option = atoi(input_buffer);
+
+    char c = getch();
+    bootloader_option = c - '0';
+    
     if (bootloader_option == 1){
-        printf("Larp Is Loading...\n");
+        printf("\nLarp Is Loading...\n");
         printf("Loading Larp OS...\n");
-
-
-
     }
     else{
         printf("Boot Entry Doesnt Exist\n");
         exit(1);
-
-
     }
-
-
 }
