ce352203b341b42986b4d394a932538924adcbe4
nickoncrack <nikosgogos137@gmail.com>
2026-09-17 19:54:28 +0300

    refactor: fix code formatting, make slight code optimizations and add an unknown command message

--- src/main.c
+++ src/main.c
@@ -5,52 +5,61 @@
 #include "include/hostname.h"
 #include "include/os_name.h"
 #include "include/split_string.h"
-int main(){
-    bootloader();
+
+
+int main() {
     char user_input[50] = "";
     char argv[6][64];
     char larper_whoami[20] = "master larper";
-    printf("Welcome To Larp OS\n");
-    while (1){
-        memset(user_input,0,sizeof(user_input));
-        for (int i = 0; i < 6; i++){
-            memset(argv[i],0,64);
-        }
 
-        printf("larp-os@%s $ ",HOSTNAME);
-       if (fgets(user_input,sizeof(user_input),stdin) == NULL){
-           break;
-       }
-        user_input[strcspn(user_input,"\n")] = '\0';
-        int argc = split_string(user_input,&argv);
-        if (strcmp(argv[0],"exit") == 0){
-            printf("Exiting Larp OS...\n");
+    bootloader();
+    printf("Welcome To Larp OS\n");
+    
+    while (1){
+        memset(user_input, 0, sizeof(user_input));
+        memset(argv, 0, 6 * 64);
+
+        printf("larp-os@%s $ ", HOSTNAME);
+        if (fgets(user_input, sizeof(user_input), stdin) == NULL){
             break;
         }
-        else if (strcmp(argv[0],"whoami") == 0){
-            printf("%s\n",larper_whoami);
-        }
-        else if (strcmp(argv[0],"version") == 0){
-            printf("%s\n",VERSION);
-        }
-        else if (strcmp(argv[0],"uname") == 0){
-            if (strcmp(argv[1],"-n") == 0){
-                printf("%s\n",HOSTNAME);
-            }
-            else{
-                printf("%s %s\n",NAME,VERSION);
+
+        user_input[strcspn(user_input, "\n")] = '\0';
+        int argc = split_string(user_input,&argv);
+
+        if (strcmp(argv[0], "exit") == 0){
+            printf("Exiting Larp OS...\n");
+            break;
+
+        } else if (strcmp(argv[0], "whoami") == 0){
+            printf("%s\n", larper_whoami);
+
+        } else if (strcmp(argv[0], "version") == 0){
+            printf("%s\n", VERSION);
+
+        } else if (strcmp(argv[0], "uname") == 0){
+            if (strcmp(argv[1], "-n") == 0){
+                printf("%s\n", HOSTNAME);
+            } else if (argc == 1) {
+                printf("%s %s\n", NAME, VERSION);
+            } else {
+                printf("uname: Invalid argument\n");
             }
 
-        }
-        else if (strncmp(argv[0],"echo",4) == 0){
-           char *echo_pointer = &user_input[5];
-           printf("%s\n",echo_pointer);
-       }
-        else if (strlen(user_input) == 0){
+        /*
+            The echo command doesn't follow the classical command line argument convention,
+            since it takes only 1 argument that can have spaces in between them without using double quotes
+            to denote a string. So using argv to check echo is not optimal.
+        */
+        } else if (strncmp(user_input, "echo ", 5) == 0) {
+            printf("%s\n", &user_input[5]);
+
+        } else if (strlen(user_input) == 0){
             continue;
+        } else {
+            printf("Unknown command: %s\n", argv[0]);
         }
-
-
     }
+
     return 0;
 }
