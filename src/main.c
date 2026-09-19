ef0abdd19526ccd44bb10621ee95f2b2176f4037
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-09-19 22:21:19 +0400

    style: change bootloader styling

--- src/main.c
+++ src/main.c
@@ -115,6 +115,7 @@ int main() {
               #endif
               // checks if you are windows or other operating systems
               // then runs clear for mac/linux and cls if you are on windows
+
         } else if (strlen(user_input) == 0){
             continue;
         } else {
