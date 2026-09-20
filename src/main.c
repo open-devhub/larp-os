43afe8d59ec1fe7922731ad9246544eaaa4e907a
nick <69575053+nickoncrack@users.noreply.github.com>
2026-09-20 11:16:56 +0300

    merge from main (#29)
    * feat: add ip a command
    
    * fix: remove echo --help text
    
    * fix: remove /etc/os-info print text
    
    * feat: add clear command
    
    * refactor: add commenting in clear command
    
    * style: change bootloader styling
    
    ---------
    
    Co-authored-by: Li Productions <172457372+goldstac@users.noreply.github.com>
    Co-authored-by: Caleb <211212128+calebephrem@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <vfs.h>
-#include <bootloader.h>
-#include <split_string.h>
+#include "include/vfs.h"
+#include "include/bootloader.h"
+#include "include/split_string.h"
+
 
 #define OS_NAME         os_info[0]
 #define OS_VERSION      os_info[1]
 #define OS_HOSTNAME     os_info[2]
-
 
 int main() {
     /*
@@ -29,14 +29,14 @@ int main() {
 
     int r = vfs_read(config_node, temp, 64);
     int os_info_split_count = split_string(temp, ';', 3, 24, os_info);
-    
+
     free(temp);
     if (os_info_split_count != 3) {
         printf("Error: failed to parse /etc/os-info: %d\n", os_info_split_count);
         return -1;
     }
 
-    char user_input[384] = { 0 };
+    char user_input[50] = "";
     char argv[6][64];
     char larper_whoami[20] = "master larper";
 
@@ -70,21 +70,21 @@ int main() {
                 printf("%s\n", OS_HOSTNAME);
             } else if (argc == 1) {
                 printf("%s %s\n", OS_NAME, OS_VERSION);
-            } else if (strcmp(argv[1], "--help") == 0) {
+            } else if (strcmp(argv[1],"--help") == 0) {
                 printf("usage : uname -[argument]\n");
                 printf("        uname           Prints OS Kernel Name\n");
                 printf("        uname -n        Prints Hostname\n");
                 printf("        uname --help    Prints This Text\n");
+
+
             } else {
                 printf("uname: Invalid argument\n");
             }
 
         } else if (strncmp(argv[0], "echo", 4) == 0) {
-            if (strcmp(argv[1],"--help") == 0){
-                printf("usage: echo [text] \n");
-            } else if (argc < 2) {
+             if (argc < 2) {
                 printf("\n");
-            } else {
+                } else {
                 for (int i = 1; i < argc; i++) {
                     fputs(argv[i], stdout);
 
@@ -94,7 +94,29 @@ int main() {
             }
 
 
-        } else if (strlen(user_input) == 0) {
+        } else if (strcmp(argv[0],"ip") == 0) {
+
+            if (strcmp(argv[1],"a") == 0){
+                printf("1: lo: <LOOPBACK,UP> mtu 65536\n");
+                printf("    inet 127.0.0.1/8 scope host lo\n");
+                printf("\n");
+                printf("2: wlan0: <BROADCAST,MULTICAST,UP> mtu 1500\n");
+                printf("    link/ether 52:54:00:12:34:56 brd ff:ff:ff:ff:ff:ff\n");
+                printf("    inet 192.168.1.67/24 scope global wlan0\n");
+            } else{
+                printf("usage : ip a\n");
+            }
+
+        } else if (strcmp(argv[0],"clear") == 0) {
+              #ifdef _WIN32
+              system("cls");
+              #else
+              system("clear");
+              #endif
+              // checks if you are windows or other operating systems
+              // then runs clear for mac/linux and cls if you are on windows
+
+        } else if (strlen(user_input) == 0){
             continue;
         } else {
             printf("Unknown command: %s\n", argv[0]);
